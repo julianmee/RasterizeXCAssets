9808b10310028e7212ecb1bfa3f5c243a28c9549
Julian Gaedecke <julian.gaedecke@affinitas.de>
2019-03-19 11:12:22 +0100

    changed to class methods

--- Sources/RasterizeXCAssets/appiconset.swift
+++ Sources/RasterizeXCAssets/appiconset.swift
@@ -3,8 +3,8 @@ import Foundation
 import PromiseKit
 
 @available(macOS 10.13, *)
-public class AppIconSet {
-    class public func build(source: String, outputURL: URL) -> Promise<Void> {
+open class AppIconSet {
+    open class func build(source: String, outputURL: URL) -> Promise<Void> {
         let fs = FileManager.default
         let worker = Rasterizer()
 
