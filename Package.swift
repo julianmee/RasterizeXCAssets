8a2594ad701ea43e9557321283f8844afb754562
Julian Gaedecke <julian.gaedecke@affinitas.de>
2019-03-19 09:45:48 +0100

    changed product to library

--- Package.swift
+++ Package.swift
@@ -16,3 +16,24 @@ let package = Package(
         .target(name: "RasterizeXCAssets", dependencies: ["Commander", "JSBridge"]),
     ]
 )
+
+
+// swift-tools-version:4.0
+import PackageDescription
+
+let pkg = Package(name: "RasterizeXCAssets")
+pkg.products = [
+    .library(name: "RasterizeXCAssets", targets: ["RasterizeXCAssets"]),
+]
+pkg.dependencies = [
+    .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
+    .package(url: "https://github.com/LinusU/JSBridge", from: "1.0.0-alpha.7"),
+]
+
+let pmk: Target = .target(name: "RasterizeXCAssets", dependencies: ["Commander", "JSBridge"])
+pmk.path = "Sources"
+
+
+pkg.targets = [
+    pmk
+]
