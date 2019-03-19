237f7ee7cfbf796c7da4e5178148d482adc7f809
Julian Gaedecke <julian.gaedecke@affinitas.de>
2019-03-19 11:01:30 +0100

    changed to class methods

--- Sources/RasterizeXCAssets/imageset.swift
+++ Sources/RasterizeXCAssets/imageset.swift
@@ -3,32 +3,34 @@ import Foundation
 import PromiseKit
 
 @available(macOS 10.13, *)
-public func imageset(source: String, outputURL: URL, size: Size) -> Promise<Void> {
-    let fs = FileManager.default
-    let worker = Rasterizer()
+public class ImageSet {
+    class public func build(source: String, outputURL: URL, size: Size) -> Promise<Void> {
+        let fs = FileManager.default
+        let worker = Rasterizer()
 
-    let sizes = [size, size.scaled(2), size.scaled(3)]
-    let images = sizes.map { worker.rasterize(source: source, width: $0.width, height: $0.height) }
+        let sizes = [size, size.scaled(2), size.scaled(3)]
+        let images = sizes.map { worker.rasterize(source: source, width: $0.width, height: $0.height) }
 
-    return firstly {
-        when(fulfilled: images)
-    }.done { images in
-        try fs.createDirectory(at: outputURL, withIntermediateDirectories: true)
+        return firstly {
+            when(fulfilled: images)
+        }.done { images in
+            try fs.createDirectory(at: outputURL, withIntermediateDirectories: true)
 
-        try images[0].write(to: outputURL.appendingPathComponent("Image@1x.png"))
-        try images[1].write(to: outputURL.appendingPathComponent("Image@2x.png"))
-        try images[2].write(to: outputURL.appendingPathComponent("Image@3x.png"))
+            try images[0].write(to: outputURL.appendingPathComponent("Image@1x.png"))
+            try images[1].write(to: outputURL.appendingPathComponent("Image@2x.png"))
+            try images[2].write(to: outputURL.appendingPathComponent("Image@3x.png"))
 
-        let contents = Contents(
-            info: Contents.Info(author: "xcode", version: 1),
-            images: [
-                Contents.Image(filename: "Image@1x.png", idiom: "universal", size: size.description, scale: "1x"),
-                Contents.Image(filename: "Image@2x.png", idiom: "universal", size: size.description, scale: "2x"),
-                Contents.Image(filename: "Image@3x.png", idiom: "universal", size: size.description, scale: "3x"),
-            ]
-        )
+            let contents = Contents(
+                info: Contents.Info(author: "xcode", version: 1),
+                images: [
+                    Contents.Image(filename: "Image@1x.png", idiom: "universal", size: size.description, scale: "1x"),
+                    Contents.Image(filename: "Image@2x.png", idiom: "universal", size: size.description, scale: "2x"),
+                    Contents.Image(filename: "Image@3x.png", idiom: "universal", size: size.description, scale: "3x"),
+                ]
+            )
 
-        let contentsURL = outputURL.appendingPathComponent("Contents.json")
-        try JSONEncoder().encode(contents).write(to: contentsURL)
+            let contentsURL = outputURL.appendingPathComponent("Contents.json")
+            try JSONEncoder().encode(contents).write(to: contentsURL)
+        }
     }
 }
