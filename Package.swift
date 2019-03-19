3607e66341f14da9dadf22af54f8b0c591434f84
Julian Gaedecke <julian.gaedecke@affinitas.de>
2019-03-19 09:46:46 +0100

    changed product to library

--- Package.swift
+++ Package.swift
@@ -1,24 +1,4 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
-    name: "rasterizeassets",
-    products: [
-        .library(name: "RasterizeAssets", targets: ["RasterizeXCAssets"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
-        .package(url: "https://github.com/LinusU/JSBridge", from: "1.0.0-alpha.7"),
-    ],
-    targets: [
-        .target(name: "RasterizeXCAssets", dependencies: ["Commander", "JSBridge"]),
-    ]
-)
-
-
-// swift-tools-version:4.0
 import PackageDescription
 
 let pkg = Package(name: "RasterizeXCAssets")
